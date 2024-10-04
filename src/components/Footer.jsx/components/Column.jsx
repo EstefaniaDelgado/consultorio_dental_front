c34f92088553d83abe1c5ce4c5dc417b5dbfd959
HansUrpay <hansurpayh@gmail.com>
2024-10-04 12:08:45 -0500

    feat(footer): add responsive for footer

--- src/components/Footer.jsx/components/Column.jsx
+++ src/components/Footer.jsx/components/Column.jsx
@@ -1,6 +1,6 @@
 const Column = ({ title, links }) => {
   return (
-    <div className="flex flex-col gap-3">
+    <div className="flex flex-col gap-3 basis-56">
       <h3 className="text-spacecadet font-extrabold text-lg pb-2">{title}</h3>
       {links.map((link, index) => (
         <a key={index} href={link.href} className="text-primary">
--- src/views/Layout/components/Subscription/Subscription.jsx
+++ src/views/Layout/components/Subscription/Subscription.jsx
@@ -1,17 +1,22 @@
 const Subscription = () => {
+  const infoPlaceholder =
+    window.innerWidth < 910
+      ? "Introduce tu correo electrónico"
+      : "Introduce tu dirección de correo electrónico";
+
   return (
     <div className="w-3/4 flex flex-col items-center justify-center bg-white border border-robineggblue dark:border-spacecadetlow rounded-[3.5rem] px-4 py-8 gap-4 dark:bg-gradient-to-r from-spacecadet to-spacecadetlow my-8 absolute top-0 transform -translate-y-28 max-w-screen-xl">
-      <h2 className="text-3xl font-extrabold text-spacecadet dark:text-robineggblue">
+      <h2 className="text-2xl text-center md:text-3xl font-extrabold text-spacecadet dark:text-robineggblue">
         Suscríbete a nuestro boletín
       </h2>
       <div className="w-3/4 relative">
         <form action="">
           <input
             type="text"
-            placeholder="Introduce tu dirección de correo electrónico"
-            className="p-6 w-full rounded-xl focus:outline-robineggblue placeholder:text-primary border border-robineggblue dark:border-spacecadetlow dark:focus:outline-none"
+            placeholder={infoPlaceholder}
+            className="p-4 mb-4 md:p-6 md:mb-0 w-full rounded-xl focus:outline-robineggblue placeholder:text-primary border border-robineggblue dark:border-spacecadetlow dark:focus:outline-none"
           />
-          <button className="absolute right-4 top-1/2 transform -translate-y-1/2 py-3 px-5 bg-robineggblue dark:bg-gradient-to-r from-spacecadet to-spacecadetlow text-white font-bold rounded-xl">
+          <button className="block mx-auto md:absolute right-4 md:top-1/2 md:transform md:-translate-y-1/2 py-3 px-5 bg-robineggblue dark:bg-gradient-to-r from-spacecadet to-spacecadetlow text-white font-bold rounded-xl">
             Suscribirse
           </button>
         </form>
