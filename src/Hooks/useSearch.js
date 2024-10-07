f76f74fca8bcc45cde73358cfcb2fbec1fd7a65d
EstefaniaDelgado <teff_64@hotmail.com>
2024-10-07 16:41:22 -0500

    search and filter dentist

--- src/Hooks/useSearch.js
+++ src/Hooks/useSearch.js
@@ -1,15 +1,19 @@
 import { useState } from 'react';
-import { ToastContainer, toast } from 'react-toastify';
+import { toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import SearchDentist from '../views/Shift/components/SearchDentist/SearchDentist';
 
 const useSearch = () => {
-  //estado para guardar el resultado del filtro
-  const [filterOptions, setFilterOptions] = useState([]);
-  //opcion seleccionada
-  const [optionSelected, setOptionSelected] = useState('');
+  //estados para guardar el resultado del filtro
+  const [filterPatients, setFilterPatients] = useState([]);
+  const [filterDentists, setFilterDentists] = useState([]);
+
+  //estados que corresponden a cada busqueda ya sea paciente u odontologo
+  const [searchPatient, setSearchPatient] = useState('');
+  const [searchDentist, setSearchDentist] = useState('');
 
   //handle que hace la busqueda y filtra
-  const handleOnSearch = (list, optionSearch, searchTerm) => {
+  const handleOnSearch = (list, optionSearch, searchTerm, type) => {
     const lowerOptionCase = optionSearch.toLowerCase();
     const filter = list.filter((item) => {
       return item[lowerOptionCase]
@@ -30,14 +34,21 @@ const useSearch = () => {
         theme: 'light',
       });
     }
-    setFilterOptions(filter);
-    setOptionSelected(searchTerm);
+    if (type === 'dentist') {
+      setFilterDentists(filter);
+      setSearchDentist(searchTerm)
+    } else {
+      setFilterPatients(filter);
+      setSearchPatient(searchTerm)
+    }
   };
 
   return {
     handleOnSearch,
-    filterOptions,
-    optionSelected,
+    filterPatients,
+    filterDentists,
+    searchPatient,
+    searchDentist
   };
 };
 
--- src/views/Shift/Shift.jsx
+++ src/views/Shift/Shift.jsx
@@ -2,17 +2,25 @@ import { useEffect, useRef, useState } from 'react';
 import SearchPatient from './components/SearchPatient/SearchPatient';
 import SearchDentist from './components/SearchDentist/SearchDentist';
 import patientService from '../../services/patientService';
+import dentistService from '../../services/dentistService';
 import FilterListPatient from './components/FilterListPatient/FilterListPatient';
+import FilterListDentist from "./components/FilterListDentist/FilterListDentist"
 import useSearch from '../../Hooks/useSearch';
-import { ToastContainer} from 'react-toastify';
+import { ToastContainer } from 'react-toastify';
 
 const Shift = () => {
   const [listPatients, setListPatients] = useState([]);
+ 
+  const [listDentists, setListDentists] = useState([]);
+ 
   //usando el hook creado
-  const { handleOnSearch, filterOptions, optionSelected } = useSearch();
+  const { handleOnSearch, filterDentists, filterPatients, searchDentist, searchPatient} = useSearch();
 
-  const refContainer = useRef();
+  const refContainerPatient = useRef();
+  const refContainerDentist = useRef();
+ 
 
+  //llama a los respectivos endpoints y trae las listas
   useEffect(() => {
     const apiListPatients = async () => {
       try {
@@ -26,19 +34,37 @@ const Shift = () => {
   }, []);
 
   useEffect(() => {
-    refContainer.current.setSelectedPatient('');
-  }, [optionSelected]);
+    const apiListDentists = async () => {
+      const response = await dentistService.getDentists();
+      setListDentists(response);
+    };
+    apiListDentists();
+  }, []);
+
+  //controla desde el padre la limpieza del estado que contien el objeto que se selecciono pra que ante una nueva busqueda muestre de nuevo la lista
+  useEffect(() => {
+    refContainerPatient.current.setSelectedPatient({});
+  }, [searchPatient]);
+
+  useEffect(()=>{
+    refContainerDentist.current.setSelectedDentist({})
+  },[searchDentist])
+
+
 
   return (
     <div className="w-full p-4 flex flex-col justify-center  items-center gap-20 md:flex-row md:justify-center md:items-center md:gap-0 ">
       <div className="relative flex flex-col flex-1  items-center justify-center gap-3">
         <SearchPatient onSearch={handleOnSearch} listPatients={listPatients} />
         <div className="absolute top-[70px] h-custom z-50">
-          <FilterListPatient filterOptions={filterOptions} ref={refContainer} />
+          <FilterListPatient filterOptions={filterPatients} ref={refContainerPatient} />
         </div>
       </div>
-      <div className="flex flex-col flex-1 items-center justify-center">
-        <SearchDentist />
+      <div className="relative flex flex-col flex-1 items-center justify-center">
+        <SearchDentist onSearch={handleOnSearch} listDentists={listDentists} />
+        <div className="absolute top-[70px] h-custom z-50">
+          <FilterListDentist filterOptions={filterDentists} ref={refContainerDentist}/>
+        </div>
       </div>
       <ToastContainer />
     </div>
