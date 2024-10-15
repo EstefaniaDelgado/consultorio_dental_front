87cf9b6e5c05ca07392221840b4b8c93d8632d27
EstefaniaDelgado <teff_64@hotmail.com>
2024-10-14 20:28:21 -0500

    feat(deploy):env variables

--- src/services/index.js
+++ src/services/index.js
@@ -1,11 +1,10 @@
 import axios from "axios";
 
-// const API_BASE_URL =
-//   process.env.NODE_ENV === "production"
-//     ? import.meta.env.VITE_APP_URL_DEPLOYMENT 
-//     : import.meta.env.VITE_APP_API_URL;
+const API_BASE_URL =
+  process.env.NODE_ENV === "production"
+    ? import.meta.env.VITE_APP_URL_DEPLOYMENT 
+    : import.meta.env.VITE_APP_API_URL;
 
-const API_BASE_URL = import.meta.env.VITE_APP_URL_DEPLOYMENT
 
 const createAxiosInstance = (baseURL) => {
   const api = axios.create({
