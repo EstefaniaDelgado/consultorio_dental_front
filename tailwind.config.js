ed9eaa94ff3c46f98d90f3e410119f42d4666975
EstefaniaDelgado <teff_64@hotmail.com>
2024-10-15 11:27:56 -0500

    styles(layout:added font family

--- tailwind.config.js
+++ tailwind.config.js
@@ -18,6 +18,9 @@ module.exports = withMT({
         spacecadetlow: "#0F2650CC",
         aliceblue: "#EEFBFA",
       },
+      fontFamily: {
+        poppins: ['Poppins', 'sans-serif'],
+      },
     },
   },
   plugins: [
