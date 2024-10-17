f7ea4a659f6ace40b3d98439f9681575ba005bc0
EstefaniaDelgado <teff_64@hotmail.com>
2024-10-17 11:19:48 -0500

    fixe(deploy): index env variables

--- src/services/index.js
+++ src/services/index.js
@@ -2,8 +2,8 @@ import axios from "axios";
 
 const API_BASE_URL =
   process.env.NODE_ENV === "production"
-    ? import.meta.env.VITE_APP_API_URL
-    : import.meta.env.VITE_APP_URL_DEPLOYMENT;
+    ?  import.meta.env.VITE_APP_URL_DEPLOYMENT
+    : import.meta.env.VITE_APP_API_URL;
 
 
 const createAxiosInstance = (baseURL) => {
