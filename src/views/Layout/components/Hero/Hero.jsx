cc2ef40725001c19e369e223b819b668999d655b
EstefaniaDelgado <teff_64@hotmail.com>
2024-10-17 08:27:21 -0500

    refactor(hero):color text in dark mode and background main image

--- src/views/Layout/components/Hero/Hero.jsx
+++ src/views/Layout/components/Hero/Hero.jsx
@@ -7,7 +7,7 @@ const Hero = () => {
   return (
     <section className="px-4 mt-5 md:flex md:flex-row-reverse md:w-full md:justify-evenly md:items-center">
       <article className="py-3 md:w-2/5">
-        <div className="relative bg-robineggblue m-auto w-80 h-[150px] max-w-48 md:max-w-none md:m-0 md:h-60 rounded-3xl">
+        <div className="relative bg-robineggblue m-auto w-80 h-[200px] max-w-48 rounded-[50px] md:max-w-none md:m-0 md:h-[310px] ">
           <img src={Dentist} alt="odontologo" className="absolute w-full bottom-0" />
         </div>
       </article>
@@ -15,18 +15,15 @@ const Hero = () => {
       <article className="max-w-80 flex flex-col gap-5 md:max-w-none md:w-2/4 md:px-8">
         <Typography
           variant="h1"
-          className="text-spacecadet font-extrabold text-3xl lg:text-5xl"
+          className="text-spacecadet font-extrabold text-3xl lg:text-5xl dark:text-white"
         >
           Haz que tu <strong className="text-robineggblue">sonrisa</strong>{" "}
           muestre lo que hay en tu interior
         </Typography>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus ipsa
-          consectetur optio vero ullam, iusto veniam voluptatibus, dolorem,
-          officia corporis dolor. Ex quae aliquid nulla nobis omnis alias et!
-          Rerum?
+        <p className="dark:text-white">
+        Ofrecemos servicios odontológicos de alta calidad, respaldados por un equipo de expertos en una amplia gama de tratamientos, comprometidos con satisfacer las necesidades individuales de cada paciente.
         </p>
-        <button className="py-3 px-6 rounded-xl bg-gradient-to-r from-spacecadet to-spacecadetlow text-robineggblue md:self-start shadow-md">
+        <button className="py-3 px-6 rounded-xl bg-white border border-robineggblue text-robineggblue md:self-start shadow-md font-medium dark:bg-gradient-to-r dark:from-spacecadet dark:to-spacecadetlow">
           Saca tu turno!
         </button>
         <div className="pb-4 flex flex-col justify-start items-start gap-2 md:flex-row lg:gap-8">
@@ -34,9 +31,9 @@ const Hero = () => {
             <img
               src={Phone}
               alt="teléfono"
-              className="bg-spacecadet p-2 rounded"
+              className="bg-white border border-robineggblue dark:bg-spacecadet p-2 rounded "
             />
-            <div>
+            <div className="dark:text-white">
               <p>Numero teléfonico</p>
               <p>01 789 290 1200</p>
             </div>
@@ -45,11 +42,11 @@ const Hero = () => {
             <img
               src={Location}
               alt="ubicación"
-              className="bg-spacecadet p-2 rounded"
+              className="bg-white border border-robineggblue dark:bg-spacecadet p-2 rounded"
             />
-            <div>
+            <div className="dark:text-white">
               <p>Ubicación</p>
-              <p>Calle San Isidro, #58-77</p>
+              <p>Calle San Isidro<h4>#58-77</h4></p>
             </div>
           </div>
         </div>
